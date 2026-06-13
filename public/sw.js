7ead10e885248536d79b385897080de338eeb59d
dildebruijnpsn-hub <dildebruijnpsn@gmail.com>
2026-06-13 17:57:12 +0200

    Refresh PWA cache for replacement icon

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'overbetuwe-afvoerservice-v20260613-3';
+const CACHE_NAME = 'overbetuwe-afvoerservice-v20260613-4';
 const STATIC_ASSETS = ['/manifest.webmanifest', '/pwa-icon.svg'];
 
 self.addEventListener('install', event => {
