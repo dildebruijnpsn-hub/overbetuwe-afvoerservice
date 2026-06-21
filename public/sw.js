0f15bcbe5202505158e28bae1d3ca6dcaa1ce438
dildebruijnpsn-hub <dildebruijnpsn@gmail.com>
2026-06-21 13:31:41 +0200

    Refresh PWA cache for workflow tools

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'overbetuwe-afvoerservice-v20260619-2';
+const CACHE_NAME = 'overbetuwe-afvoerservice-v20260621-1';
 const STATIC_ASSETS = ['/manifest.webmanifest', '/pwa-icon.svg'];
 
 self.addEventListener('install', event => {
